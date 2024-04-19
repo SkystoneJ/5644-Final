09d2a96a2da8580207c95d3316ecfa4bd1affc6c
NinghangJ <jiaot2275@uwec.edu>
2024-04-18 20:39:23 -0400

    1

--- trainmodel.py
+++ trainmodel.py
@@ -7,7 +7,7 @@ X_train = pd.read_csv('trainX.csv')
 y_train = pd.read_csv('trainy.csv')
 X_test = pd.read_csv('testX.csv')
 y_test = pd.read_csv('testy.csv')
-# 初始化随机森林回归器
+
 regressor = RandomForestRegressor(n_estimators=400, random_state=42, max_depth=20, min_samples_split=3,
                                   min_samples_leaf=1, n_jobs=-1)
 
